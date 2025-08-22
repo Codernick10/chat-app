ab16f721e6ffb109f7360159fdbb5bba701c0c47
Codernick10 <nitesh.striker321@gmail.com>
2025-08-22 23:33:21 +0530

    chat page added

--- front-end/front-chat/src/component/ChatPage.jsx
+++ front-end/front-chat/src/component/ChatPage.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { MdAttachFile, MdSend } from "react-icons/md";
+
+
+
 
 const ChatPage = () => {
+   const [messages, setMessages] = useState([
+  {
+  content: "Hello ?",
+  sender: "Nitesh",
+  },
+  {
+  content: "Hello ?",
+  sender: "shreya",
+  },
+  {
+  content: "Hii ?",
+  sender: "shubham",
+  },
+
+]);
+
+const [input, setInput] = useState("");
+const inputRef = useRef(null);
+const chatBoxRef = useRef(null);
+const [stompClient, setStompClient] = useState(null);
+const [roomId, setRoomId] = useState("");
+const [currentUser] = useState("Nitesh");
+   
   return (
     <div className="">
         {/* this is a header */}
@@ -25,18 +52,49 @@ const ChatPage = () => {
                     Leave Room
                  </button>
             </div>    
-        </header> 
+        </header>
+
+        <main className="py-20 px-10 w-2/3 dark:bg-slate-600 mx-auto h-screen overflow-auto ">
+           
+           
+               {messages.map((message, index) => (
+                  
+                 <div key={index} className={`flex ${message.sender === currentUser ? "justify-end":"justify-start" }`}>
+                   <div className={`my-2 ${message.sender===currentUser ? 'bg-green-800' : 'bg-gray-800'} p-2 max-w-xs rounded`}>
+                      <div className="flex flex-row gap-2">
+                        <img className="h-10 w-10" src={'https://avatar.iran.liara.run/public/49'} alt="" />
+                        <div className=" flex flex-col gap-1 ">
+                          <p className="text-sm font-bold">{message.sender}</p>
+                          <p>{message.content}</p>
+                        </div>
+                      </div>
+                    </div> 
+                 </div>
+
+                    
+                ))}
+             </main>
 
         {/* Input message container */}
-        <div className="fixed bottom-2 w-full h-16">
-        <div className="h-full rounded w-2/3 mx-auto dark:bg-gray-900">
-             <input type="text" placeholder="Type your message here..." className="dark:border-gray-900 px-3 py-2 rounded w-full h-full" />
+        <div className="fixed bottom-4 w-full h-16">
+          <div className="h-full pr-10 gap-4 flex items-center justify-between rounded-full w-1/2 mx-auto dark:bg-gray-900">
+                 <input type="text" 
+                placeholder="Type your message here..." 
+                 className=" w-full dark:border-gray-600 b dark:bg-gray-800 px-5 py-2 rounded-full h-full focus:outline-none" />
+              <div className="flex gap-1"> 
+                 <button className="dark:bg-purple-600 h-10 w-10  flex justify-center items-center rounded-full ">
+                  <MdAttachFile size={20}/>
 
+                 </button><button className="dark:bg-green-600 h-10 w-10  flex justify-center items-center rounded-full ">
+                  <MdSend size={20}/>
+                 </button>
+
+                </div>
         </div>    
       </div>
           
     </div>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
