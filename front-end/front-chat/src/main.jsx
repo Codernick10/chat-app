ad88cf9aad0ba465298f37eb2b602c08aa1f0bf0
Codernick10 <nitesh.striker321@gmail.com>
2025-09-01 22:52:05 +0530

    frontend upadated

--- front-end/front-chat/src/main.jsx
+++ front-end/front-chat/src/main.jsx
@@ -5,12 +5,16 @@ import './index.css';
 import { BrowserRouter } from "react-router";
 import AppRoutes from './config/Routes.jsx';
 import { Toaster } from 'react-hot-toast';
+import { ChatProvider } from './context/ChatContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Toaster position="top-center" />
-        <AppRoutes/>
+      <ChatProvider>
+            <AppRoutes/>
+      </ChatProvider>
+        
     </BrowserRouter>
   
   </StrictMode>
